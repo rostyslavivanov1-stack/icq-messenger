c752f53752ad04ca2a54ed2f93401c5de3e928a1
rostyslavivanov1-stack <rostyslav.ivanov1@nure.ua>
2026-05-29 20:49:01 +0300

    feat: ClientApp

--- src/main/java/ua/knure/icq/client/ClientApp.java
+++ src/main/java/ua/knure/icq/client/ClientApp.java
@@ -1,7 +1,43 @@
 package ua.knure.icq.client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientApp {
+    private static final String SERVER_HOST = "localhost";
+    private static final int SERVER_PORT = 12345;
     public static void main(String[] args) {
-        System.out.println("--- ICQ КЛІЄНТ ЗАПУЩЕНО ---");
+        System.out.println("[Launching ICQ Client]");
+        try (Socket socket = connectToServer(SERVER_HOST, SERVER_PORT)) {
+            System.out.println("Connected to ICQ server.");
+            handleUserInput(socket);
+        } catch (IOException exception) {
+            System.out.println("Client error.");
+            exception.printStackTrace();
+        }
+    }
+
+    private static Socket connectToServer(String host, int port) throws IOException {
+        System.out.println("Connecting to ICQ server " + host + ":" + port + "...");
+        return new Socket(host, port);
+    }
+
+    private static void handleUserInput(Socket socket) throws IOException {
+            try (PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                Scanner scanner = new Scanner(System.in)
+            ) {
+                System.out.println("Type message or /exit to quit.");
+
+                while (true) {
+                    String message = scanner.nextLine();
+                    if (message.equalsIgnoreCase("/exit")) {
+                        System.out.println("Closing client...");
+                    break;
+                }
+                output.println(message);
+            }
+        }
     }
 }
