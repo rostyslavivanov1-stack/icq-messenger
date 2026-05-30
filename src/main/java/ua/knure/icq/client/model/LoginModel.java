fedc549926d596ddbfed02850664867ea85dadfe
rostyslavivanov1-stack <rostyslav.ivanov1@nure.ua>
2026-05-30 16:38:01 +0300

    feat: add LoginModel

--- src/main/java/ua/knure/icq/client/model/LoginModel.java
+++ src/main/java/ua/knure/icq/client/model/LoginModel.java
@@ -1,5 +1,27 @@
 package ua.knure.icq.client.model;
 
 public class LoginModel {
-    
+    private String serverIp;
+    private String username;
+
+    public String getServerIp() {
+        return serverIp;
+    }
+
+    public void setServerIp(String serverIp) {
+        this.serverIp = serverIp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isValid() {
+        return serverIp != null && !serverIp.isBlank()
+                && username != null && !username.isBlank();
+    }
 }
