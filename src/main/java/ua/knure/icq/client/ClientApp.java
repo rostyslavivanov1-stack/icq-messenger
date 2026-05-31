1ec5bc579053402f2fd9514d436afec4880d20b6
rostyslavivanov1-stack <rostyslav.ivanov1@nure.ua>
2026-05-31 11:41:14 +0300

    style: JavaFX login view design

--- src/main/java/ua/knure/icq/client/ClientApp.java
+++ src/main/java/ua/knure/icq/client/ClientApp.java
@@ -8,13 +8,12 @@ import javafx.stage.Stage;
 public class ClientApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
-                ClientApp.class.getResource("/ua/knure/icq/client/view/login-view.fxml")
-        );
-
+        FXMLLoader loader = new FXMLLoader(ClientApp.class.getResource("/ua/knure/icq/client/view/login-view.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setTitle("ICQ Login");
         stage.setScene(scene);
+        stage.setWidth(900);
+        stage.setHeight(500);
         stage.show();
     }
 
